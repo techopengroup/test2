fcc126a7ebd63778ac0833aed5885a07f257fc78
techopengroup <techopengroup@gmail.com>
2023-04-26 22:11:06 +0530

    TestController file submit

---
+++ test1/src/main/java/com/demo/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.demo.controller;
+
+public class TestController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just Github Commit");
+
+	}
+
+}
